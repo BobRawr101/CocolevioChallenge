6abcab6c64be3b1b978ea49aaba0bfc093e02f95
BobRawr101 <drizzt199600@gmail.com>
2017-08-23 17:36:40 -0500

    print possible combos

--- test.py
+++ test.py
@@ -13,4 +13,27 @@ import pandas as pd
         * price/item does not matter, just end profit
 '''
 
+
 def main():
+
+    # amount of inventory seller has 
+    stock = 23
+
+    input = pd.read_csv('input.csv', index_col='Company')
+    print(input)
+
+    lists = subset_sum(input['Amount'], stock)
+    for list in lists:
+        print(list)
+
+
+def subset_sum(numbers, target, partial=[], partial_sum=0):
+    if partial_sum == target:
+        yield partial
+    if partial_sum >= target:
+        return
+    for i, n in enumerate(numbers):
+        remaining = numbers[i + 1:]
+        yield from subset_sum(remaining, target, partial + [n], partial_sum + n)
+
+main()
