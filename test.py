35868494d1384b972ff963b74dac05271b7f3345
BobRawr101 <drizzt199600@gmail.com>
2017-08-23 17:07:53 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+import pandas as pd 
+
+
+'''
+    author: Sitansh Rajput
+    assumptions:
+        * data will be given in a .csv format similar to one presented in .png
+        * we must find the list of companies that results in the highest sale
+        * price/item does not matter, just end profit
+'''
+
+def main():
